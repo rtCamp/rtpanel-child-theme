995576d2994a15d92c71c70bc6edb901ab42cfb6
Sagar Jadhav <sagar.jadhav@rtcamp.com>
2013-04-18 17:12:38 +0530

    Update slide show code

--- lib/rtp-slideshow.php
+++ lib/rtp-slideshow.php
@@ -36,34 +36,37 @@ if ( function_exists( 'add_image_size' ) ) {
  *
  * @since rtPanelChild 1.0
  */
-function rtp_get_cycle_slider( $slide_number = 5, $content_length = 300, $show_title = true, $show_excerpt = true, $show_navigation = true, $show_pagination = true ) {
+function rtp_get_cycle_slider( $slide_number = 5, $content_length = 200, $show_title = true, $show_excerpt = true, $show_navigation = true, $show_pagination = true ) {
     $slider_q = new WP_Query( array( 'ignore_sticky_posts' => 1, 'posts_per_page' => $slide_number, 'order' => 'DESC' ) );
-    if ( !count( $slider_q ) ) {
-        $slider_q = new WP_Query( array( 'ignore_sticky_posts' => 1, 'posts_per_page' => $slide_number, 'order' => 'DESC' ) );
-    }
+
     $slider_image = '';
     $slider_pagination = false;
     $slider_html = '<div id="rtp-cycle-slider">';
+
     if ( $slider_q->have_posts() ) {
         $slider_html .= '<div class="cycle-slideshow rtp-cycle-slider-container" data-cycle-slides="div" data-cycle-log="false" data-cycle-timeout="3000" data-cycle-prev="#rtp-prev-cycle" data-cycle-next="#rtp-next-cycle">';
+
         while ( $slider_q->have_posts() ) { $slider_q->the_post();
+
             if ( has_post_thumbnail() ) {
-                $image_details = wp_get_attachment_image_src( get_post_thumbnail_id(), 'rtp-slider' );
+                $image_details = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
                 $slider_image = $image_details[0];
             }
-            
+
             if ( $slider_image ) {
                 $slider_html .= '<div class="cycle-slides">';
                     $slider_html .= '<a href="' . get_permalink() .'" title="'.  esc_attr( get_the_title() ) . '"><img class="cycle-slider-img" src ="' . $slider_image . '" alt="' . esc_attr( get_the_title() ) . '" /></a>';
                     $slider_html .= ( $show_title ) ? '<h1><a href="' . get_permalink() .'" title="'.  get_the_title().'" rel="bookmark">' . ( ( strlen( get_the_title() ) > 50 ) ? substr( get_the_title(), 0, 50 ) . "..." : get_the_title() ) . '</a></h1>' : '';
                     $slider_html .= ( $show_excerpt ) ? ( ( strlen( get_the_content() ) > $content_length ) ? wp_html_excerpt( get_the_content(), $content_length ) . '...' : wp_html_excerpt( get_the_content(), $content_length ) ) : '';
-
                 $slider_html .= '</div>';
             }
+
             $slider_pagination = true;
         }
+
         $slider_html .= '</div>';
     }
+
     wp_reset_postdata();
 
     /* Uncomment following line if using pagination in the slider */
@@ -72,6 +75,8 @@ function rtp_get_cycle_slider( $slide_number = 5, $content_length = 300, $show_t
             $slider_html .= ( $show_navigation ) ? '<a href="#" id="rtp-prev-cycle" class="previous-cycle"><span>'. __( 'Prev', 'rtPanel' ) . '</span></a><a href="#" id="rtp-next-cycle" class="next-cycle"><span>'. __( 'Next', 'rtPanel' ) . '</span></a>' : '';
         $slider_html .= '</div>';
     }
+
     $slider_html .= '</div>';
+
     echo $slider_html;
 }
