b9486529c8874677e6083b84d13e1ced7f02eb18
Sagar Jadhav <sagarjadhav27@gmail.com>
2013-12-16 12:08:49 +0530

    Update with parent theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,24 +2,27 @@
 module.exports = function(grunt) {
 
     // load all grunt tasks matching the `grunt-*` pattern
+    // Ref. https://npmjs.org/package/load-grunt-tasks
     require('load-grunt-tasks')(grunt);
 
     grunt.initConfig({
-
-        // watch for changes and trigger compass, jshint, uglify and livereload
+        
+        // Watch for hanges and trigger compass, jshint, uglify and livereload
+        // Ref. https://npmjs.org/package/grunt-contrib-watch
         watch: {
             compass: {
                 files: ['assets/rtpanel/**/*.{scss,sass}'],
                 tasks: ['compass']
             },
-            
+
             livereload: {
-                options: { livereload: true },
+                options: {livereload: true},
                 files: ['style.css', 'js/*.js', '*.html', '*.php', 'img/**/*.{png,jpg,jpeg,gif,webp,svg}']
             }
         },
-
-        // compass and scss
+        
+        // SCSS and Compass
+        // Ref. https://npmjs.org/package/grunt-contrib-compass
         compass: {
             dist: {
                 options: {
@@ -29,54 +32,94 @@ module.exports = function(grunt) {
             }
         },
 
-        // image optimization
+        // Fontello Icons
+        fontello: {
+            dist: {
+                options: {
+                  config: 'assets/fontello/config.json',
+                  fonts: 'assets/fontello/font',
+                  styles: 'assets/fontello/css',
+                  scss: true,
+                  // zip: 'test/output',
+                  force: true
+                }
+            }
+        },
+        
+        // Image Optimization
+        // Note : This needs to improve
+        // Ref. https://npmjs.org/package/grunt-contrib-imagemin
         imagemin: {
             dist: {
                 options: {
                     optimizationLevel: 7,
                     progressive: true
                 },
-                files: [{
+
+                files: {
                     expand: true,
                     cwd: 'img/',
-                    src: '**/*',
-                    dest: 'img/'
-                }]
+                    src: ['**/*'],
+                    dest: 'img/test/'
+                }
             }
         },
 
-        // deploy via rsync
-        deploy: {
+        // WordPress Deployment
+        // Ref. https://npmjs.org/package/grunt-wordpress-deploy
+        wordpressdeploy: {
             options: {
-                src: "./",
-                args: ["--verbose"],
-                exclude: ['.git*', 'node_modules', '.sass-cache', 'Gruntfile.js', 'package.json', '.DS_Store', 'README.md', 'config.rb'],
-                recursive: true,
-                syncDestIgnoreExcl: true
+                backup_dir: "backups/",
+                rsync_args: ['-avz'],
+                exclusions: ['Gruntfile.js', '.git/', 'tmp/*', 'backups/', 'wp-config.php', 'composer.json', 'composer.lock', 'README.md', '.gitignore', 'package.json', 'node_modules', '.sass-cache', 'npm-debug.log', '.scss-cache']
             },
+
+            local: {
+                "title": "local",
+                "database": "database_name",
+                "user": "database_username",
+                "pass": "database_password",
+                "host": "database_host",
+                "url": "http://local_url",
+                "path": "/local_path"
+            },
+
             staging: {
-                options: {
-                    dest: "~/path/to/theme",
-                    host: "user@host.com"
-                }
+                "title": "staging",
+                "database": "database_name",
+                "user": "database_username",
+                "pass": "database_password",
+                "host": "database_host",
+                "url": "http://staging_url",
+                "path": "/staging_path",
+                "ssh_host": "user@staging_host"
             },
-            production: {
-                options: {
-                    dest: "~/path/to/theme",
-                    host: "user@host.com"
-                }
+
+            final: {
+                "title": "final",
+                "database": "database_name",
+                "user": "database_username",
+                "pass": "database_password",
+                "host": "database_host",
+                "url": "http://final_url",
+                "path": "/staging_path",
+                "ssh_host": "user@staging_host"
             }
         }
-
     });
 
     // rename tasks
-    grunt.renameTask('rsync', 'deploy');
+    // grunt.renameTask('rsync', 'deploy');
 
     // register task
-    grunt.registerTask('default', ['watch']);
-    
-    // register task
-    grunt.registerTask('imagemin', ['imagemin']);
+    // grunt.registerTask('imagemin', ['imagemin']);
 
+    // Fontello Fonts
+    // grunt.registerTask('iconFonts', ['fontello']);
+
+    // WordPress Deploy
+    //grunt.registerTask('default', ['wordpressdeploy']);
+
+    // register task
+    grunt.registerTask('default', ['fontello', 'watch']);
 };
