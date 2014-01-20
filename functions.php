d7b7ef84d48c9ee09c02e5c9724a072577aa8eb6
Manish Songirkar <manish.songirkar@rtcamp.com>
2014-01-20 16:17:37 +0530

    Fixed the path issue

--- functions.php
+++ functions.php
@@ -11,8 +11,8 @@
     define( 'RTP_CHILD_CSS', get_stylesheet_directory_uri() . '/css' );
     define( 'RTP_CHILD_JS', get_stylesheet_directory_uri() . '/js' );
     define( 'RTP_CHILD_IMG', get_stylesheet_directory_uri() . '/img' );
-    define( 'RTP_CHILD_ASSETS_URL', get_template_directory_uri() . '/assets' );
-    define( 'RTP_CHILD_BOWER_COMPONENTS_URL', get_template_directory_uri() . '/assets/foundation/bower_components' );
+    define( 'RTP_CHILD_ASSETS_URL', get_stylesheet_directory_uri() . '/assets' );
+    define( 'RTP_CHILD_BOWER_COMPONENTS_URL', get_stylesheet_directory_uri() . '/assets/foundation/bower_components' );
 
 // Includes PHP files located in 'lib' folder
 foreach( glob ( get_stylesheet_directory() . "/lib/*.php" ) as $lib_filename ) {
